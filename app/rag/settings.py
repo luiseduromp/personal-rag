5696f8d7b7db3ab2240a301667ef2ef1cf7e1aa0
Luis Romero <luiseduromp@hotmail.com>
2025-08-13 19:25:17 +0200

    🛠️ Fixes to settings to improve accuracy

--- app/rag/settings.py
+++ app/rag/settings.py
@@ -10,11 +10,12 @@ LLM_MODEL = "gpt-4o-mini"
 TEMPERATURE = 0.5
 EMBEDDINGS_MODEL = "text-embedding-3-small"
 
-CHUNK_SIZE = 500
-CHUNK_OVERLAP = 100
+CHUNK_SIZE = {"en": 350, "es": 460}
+CHUNK_OVERLAP = {"en": 50, "es": 60}
 
 _raw_base_dir = os.getenv("BASE_DIR", "app")
 BASE_DIR = Path(_raw_base_dir).resolve()
+DATA_DIR = os.path.join(BASE_DIR, "docs")
 DATABASE_DIR = os.path.join(BASE_DIR, "chroma_db")
 DEFAULT_COLLECTION = "luiseduromp_rag"
 
